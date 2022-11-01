f8160475eaca8905e8bbdc1b46d3c2b910b39806
Andrew Plummer <andrew.plummer@isotoma.com>
2022-11-01 16:03:00 +0000

    Use node 14

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ export class AllowConnectionsToECSServiceFromNetworkLoadBalancerProvider extends
         this.provider = new customResource.Provider(this, 'allow-connections-to-ecs-service-from-network-load-balancer', {
             onEventHandler: new lambda.Function(this, 'allow-connections-to-ecs-service-from-network-load-balancer-event', {
                 code: lambda.Code.fromAsset(path.join(__dirname, 'provider')),
-                runtime: lambda.Runtime.NODEJS_12_X,
+                runtime: lambda.Runtime.NODEJS_14_X,
                 handler: 'index.onEvent',
                 timeout: cdk.Duration.minutes(5),
                 initialPolicy: [
